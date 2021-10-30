23e0713a476eca0a9d7d84404d1939bbb4925a81
a7med7asan15 <s-ahmed.hasan@zewailcity.edu.eg>
2021-10-30 13:01:23 +0200

    certbot and upload

--- rest/middlewares/uploadImage.js
+++ rest/middlewares/uploadImage.js
@@ -1,30 +1,13 @@
 const multer = require('multer');
-var multerGoogleStorage = require("multer-google-storage");
 
-var aws = require('aws-sdk');
-var multerS3 = require('multer-s3');
 const path = require('path');
-const spacesEndpoint = new aws.Endpoint(process.env.doEndpoint);
 
 
-
-var s3 = new aws.S3({
-  //endpoint: spacesEndpoint,
-  accessKeyId:process.env.asID,
-  secretAccessKey:process.env.asSecretID,
-  //bucket: process.env.asBucket,
-})
-
-const storage = multerS3({
-  s3: s3,
-  acl: 'public-read', // this should set the file to Public
-  bucket: process.env.asBucket,
-  metadata: function (req, file, cb) {
-    cb(null, {fieldName: file.fieldname});
-  },
-  key: function (req, file, cb) {
-    cb(null,path.basename(file.originalname ,path.extname(file.originalname))+ '-' + Date.now() + path.extname(file.originalname))
-  }
+const storage = multer.diskStorage({
+  destination: './public/images/', 
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); 
+  }, 
 })
 // const storage = multerGoogleStorage.storageEngine({
 //   acl:"publicread"
