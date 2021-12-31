211108968fba26d063bfff47000870ace7c77561
a7med7asan15 <s-ahmed.hasan@zewailcity.edu.eg>
2021-12-31 11:31:31 +0200

    upload image

--- rest/middlewares/uploadImage.js
+++ rest/middlewares/uploadImage.js
@@ -1,37 +1,51 @@
 const multer = require('multer');
-const fs = require('fs')
 
+var aws = require('aws-sdk');
+var multerS3 = require('multer-s3');
 const path = require('path');
+const spacesEndpoint = new aws.Endpoint(process.env.doEndpoint);
 
 
-const storage = multer.diskStorage({
-  destination:(req, file, cb)=>{
-    cb(null, 'Images')
-  }, 
-  filename: function(req, file, cb){
-    console.log(file)
-    cb(null, Date.now() + path.extname(file.originalname)); 
-  }, 
+
+var s3 = new aws.S3({
+  //endpoint: spacesEndpoint,
+  accessKeyId:process.env.asID,
+  secretAccessKey:process.env.asSecretID,
+  //bucket: process.env.asBucket,
 })
 
+const storage = multerS3({
+  s3: s3,
+  acl: 'public-read', // this should set the file to Public
+  bucket: process.env.asBucket,
+  metadata: function (req, file, cb) {
+    cb(null, {fieldName: file.fieldname});
+  },
+  key: function (req, file, cb) {
+    cb(null,path.basename(file.originalname ,path.extname(file.originalname))+ '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+// const storage = multerGoogleStorage.storageEngine({
+//   acl:"publicread"
+// })
 
 var upload = multer({ storage: storage }); 
 
-// var deleteImage = 
-//   fs.unlink(image_path, (err) => {
-//     if (err) {
-//       console.error(err)
-//       return
-//     }
-  
-//     //file removed
-//   })
+var deleteImage = async(image)=>{
+  var params = {
+    Bucket: process.env.asBucket,
+    Key: image
+};
+  await s3.deleteObject(params, function(err, data) {
+    if (err) return err;
+    else     return true;
+  });
 
-
+}
 
 module.exports = {
-  upload
-  //,deleteImage
+  upload,
+  deleteImage
 
 }; 
 
